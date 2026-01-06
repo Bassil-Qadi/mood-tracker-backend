c83ff47bc8ef66bffa9aa3f0976347fb79302049
Bassil-Qadi <bassilalqadi65@gmail.com>
2026-01-07 00:42:09 +0300

    Small fix in update profile

--- src/controllers/UserProfileController.ts
+++ src/controllers/UserProfileController.ts
@@ -2,30 +2,32 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/User';
 
 export const updateUserData = async (
-    req: Request, res: Response, next: NextFunction
-  ) => {
-    try {
-      const { userId, name } = req.body;
-  
-      const user = await User.findByIdAndUpdate(
-        userId,
-        { name },
-        { new: true, runValidators: true }
-      );
-  
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          message: "User Not Found",
-        });
-      }
-  
-      return res.status(200).json({
-        success: true,
-        message: "User updated successfully",
-        data: user,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  try {
+    const { userId, name } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { name },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
       });
-    } catch (error) {
-      next(error as Error);
     }
-  };
+
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    return next(error as Error);
+  }
+};
